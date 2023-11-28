4c407945a5ba2cdb7164ff7a72489400b52582ab
felipe <felipebortolini2001@gmail.com>
2023-11-28 20:04:19 -0300

    TP2 pronto

--- alocador_memoria/alocador_memoria.c
+++ alocador_memoria/alocador_memoria.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <unistd.h>
 
-#define MEMORY_SIZE 1024  // Size of the fixed memory block
+/* Maximum number of memory blocks that can be allocated */
+#define MAX_ALLOCATIONS 1000
+
+struct allocation_t{
+    void *ptr;
+    unsigned int size;
+} allocations[MAX_ALLOCATIONS];
 
 #pragma pack(push, 1)
 struct mem_block {
@@ -12,21 +20,20 @@ struct mem_block {
 };
 #pragma pack(pop)
 
-// char* memory = NULL;  // Pointer to the memory block
+unsigned int maxMemory;
 struct mem_block* head = NULL;  // Head of the linked list
 
 void* smalloc(size_t size) {
 
     if (!head) {
-        // head = (struct mem_block *)memory;
-        head = sbrk(MEMORY_SIZE);
+        char* memory = sbrk(maxMemory);
+        head = (struct mem_block *)memory;
         if (head == (void*)-1) {
             perror("sbrk");
             return 1;  // Error allocating memory
         }
         head->is_free = 1;
-        head->size = MEMORY_SIZE - sizeof(struct mem_block);
-        // head->mem_ptr = head + 1;
+        head->size = maxMemory - sizeof(struct mem_block);
         head->mem_ptr = head + sizeof(struct mem_block);
         head->next = NULL;
     }
@@ -34,14 +41,11 @@ void* smalloc(size_t size) {
     struct mem_block* curr = head;
 
     while (curr) {
-        // if (curr->is_free && curr->next && curr->next->is_free) {
-        //     curr->next = curr->next->next;
-        //     curr->size = curr->size + sizeof(struct mem_block) + curr->next->size;
-        // }
+        if (curr->is_free && curr->next && curr->next->is_free) {
+            curr->next = curr->next->next;
+            curr->size = curr->size + sizeof(struct mem_block) + curr->next->size;
+        }
         if (curr->is_free && curr->size >= size + sizeof(struct mem_block)) {
-            // if (curr->size > size + sizeof(struct mem_block)) {
-            // if (curr->size >= size) {
-                // Split the block
             if((int)curr->size - (int)size <= 0) {
                 curr->next = NULL;
             } else {
@@ -54,7 +58,6 @@ void* smalloc(size_t size) {
             }
             curr->size = size;
             curr->is_free = 0;
-            // }
             return curr->mem_ptr;
         }
         curr = curr->next;
@@ -67,10 +70,10 @@ void* smalloc(size_t size) {
 void sfree(void* ptr) {
     struct mem_block* curr = head;
     while (curr) {
-        // if (curr->is_free && curr->next && curr->next->is_free) {
-        //     curr->size = curr->size + sizeof(struct mem_block) + curr->next->size;
-        //     curr->next = curr->next->next;
-        // }
+        if (curr->is_free && curr->next && curr->next->is_free) {
+            curr->size = curr->size + sizeof(struct mem_block) + curr->next->size;
+            curr->next = curr->next->next;
+        }
         if (curr->mem_ptr == ptr) {
             curr->is_free = 1;
             // Coalesce adjacent free blocks
@@ -78,57 +81,52 @@ void sfree(void* ptr) {
                 curr->size += sizeof(struct mem_block) + curr->next->size;
                 curr->next = curr->next->next;
             }
-            // if (curr->next && curr->next->next && curr->next->next->is_free) {
-            //     curr->next->size += sizeof(struct mem_block) + curr->next->next->size;
-            //     curr->next = curr->next->next->next;
-            // }
             break;
         }
         curr = curr->next;
     }
 }
 
-// void sfree(void* ptr) {
-//     struct mem_block* curr = head;
-//     while (curr) {
-//         if (curr->mem_ptr == ptr) {
-//             curr->is_free = 1;
-//             // Coalesce adjacent free blocks
-//             while (curr->next && curr->next->is_free) {
-//                 curr->size += sizeof(struct mem_block) + curr->next->size;
-//                 curr->next = curr->next->next;
-//             }
-//             break;
-//         }
-//         curr = curr->next;
-//     }
-// }
+int main(int argc, char *argv[]) {
+    unsigned int currentMemory = 0;
+    int allocCount = 0;
+    unsigned int size;
 
-int main() {
-    // Allocate the fixed-size memory block using sbrk
-    // memory = sbrk(MEMORY_SIZE);
-    // if (memory == (void*)-1) {
-    //     perror("sbrk");
-    //     return 1;  // Error allocating memory
-    // }
+    if (argc < 2){
+        printf("Usage: %s <max memory in bytes>\n", argv[0]);
+        return 1;
+    }
 
-    // Simulating memory allocation and deallocation
-    void* ptr1 = smalloc(200);  // Allocate 200 bytes
-    void* ptr2 = smalloc(300);  // Allocate 300 bytes
-    // void* ptr3 = smalloc(167);  // Allocate 200 bytes
-    // void* ptr4 = smalloc(5);
-    // sfree(ptr4);
-    // void* ptr5 = smalloc(213);  // Allocate 300 bytes
-    // sfree(ptr1);  // Free the first block
-    void* ptr3 = smalloc(167);  // Allocate 150 bytes (should reuse the freed block)
-    sfree(ptr2);  // Free the second block
-    void* ptr4 = smalloc(5);  // Allocate 500 bytes (should coalesce with the freed block)
-    void* ptr5 = smalloc(183);
-    void* ptr6 = smalloc(1);
-    void* ptr7 = smalloc(7);
-    void* ptr8 = smalloc(183);
-    // void* ptr4 = smalloc(5);
-    // void* ptr5 = smalloc(213);
+    maxMemory = atoi(argv[1]);
+
+    srand(time(NULL));
+
+    while(1){
+        if (rand() % 2 == 0 && allocCount < MAX_ALLOCATIONS - 1){
+            /* Allocate memory */
+            size = rand() % 100;
+            if (currentMemory + size > maxMemory){
+                printf("Out of memory\n");
+                continue;
+            }
+            /* Change malloc() by smalloc() */
+            allocations[allocCount].ptr = smalloc(size);
+            allocations[allocCount].size = size;
+            currentMemory += size;
+            allocCount++;
+            printf("Allocated %u bytes, total %u, position %i\n", size, currentMemory, allocCount);
+        }else{
+            /* Free memory */
+            if (allocCount > 0){
+                allocCount--;
+                size = allocations[allocCount].size;
+                currentMemory -= size;
+                /* Change free() by sfree() */
+                sfree(allocations[allocCount].ptr);
+                printf("Free %u bytes, total %u, position %i\n", size, currentMemory, allocCount);
+            }
+        }
+    }
 
     return 0;
 }
